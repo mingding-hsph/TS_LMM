3177a2b96e5f1c8af761c932ca59fe03bc10b530
Ming Ding <ming_ding@med.unc.edu>
2024-06-15 23:26:56 -0400

    Update example.TS_LMM.r

--- example.TS_LMM.r
+++ example.TS_LMM.r
@@ -7,20 +7,20 @@ source('./TS_LMM.macro.r')
 new_data<-read.csv("./data.csv", row.names = NULL)
 
 ##read in corr_snps, which is the correlation between snps
-##can be obtained using LDmatrix Tool in LDlink
+##ld can be obtained using LDmatrix Tool in LDlink. Please refer to our paper how to obtain correlation between snps based on LD
 
-ld_snps_readin<-read.csv("./corr_snps.csv", row.names = NULL)
-ld_snps<- ld_snps_readin[,-1]  ##exclude the first column which is a character variable
+corr_snps_readin<-read.csv("./corr_snps.csv", row.names = NULL)
+corr_snps<- corr_snps_readin[,-1]  ##exclude the first column which is a character variable
 
 ##check and make sure that data are numeric and has 20 rows and 20 columns
 ##where 20 is the number of genetic variants
-summary(ld_snps)  
-nrow(ld_snps) 
-ncol(ld_snps) 
+summary(corr_snps)  
+nrow(corr_snps) 
+ncol(corr_snps) 
 
 ##calculate correlation between summary statistics of risk factors 
 
-ld_x<-cor(new_data[, c('bx1', 'bx2', 'bx3')])
+corr_x<-cor(new_data[, c('bx1', 'bx2', 'bx3')])
   
 ##apply TS_LMM
   
@@ -29,9 +29,9 @@ ld_x<-cor(new_data[, c('bx1', 'bx2', 'bx3')])
     betaY=new_data$by, 
     betaX_se=cbind(new_data$bx_se1, new_data$bx_se2, new_data$bx_se3),
     betaY_se=new_data$by_se,
-    ld_snps_tslmm =ld_snps,
-    ld_X_tslmm =ld_x,
-    loop_rem=50,
+    corr_snps_tslmm =corr_snps,
+    corr_X_tslmm =corr_x,
+    loop_rem=500,
     cutoff_rem=0.00001 
   )
   
